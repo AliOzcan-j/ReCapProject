023afd5a3824848bbdc53372b753f4ab4a8f69b1
SocialCat5506 <ali-ozcan93@hotmail.com>
2021-12-15 22:55:59 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=1990,DailyPrice=500,Description="Hızlı ve Öfkeli"},
+                new Car{Id=2,BrandId=1,ColorId=2,ModelYear=2020,DailyPrice=500,Description="Aile"},
+                new Car{Id=3,BrandId=2,ColorId=2,ModelYear=2023,DailyPrice=500,Description="Spor"},
+                new Car{Id=4,BrandId=3,ColorId=3,ModelYear=2028,DailyPrice=500,Description="Pikup"},
+                new Car{Id=5,BrandId=3,ColorId=3,ModelYear=2020,DailyPrice=500,Description="Offroad"},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
