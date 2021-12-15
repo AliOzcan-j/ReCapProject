8e0a799aee9c549650be3ce92a4445fdaf920c7d
SocialCat5506 <ali-ozcan93@hotmail.com>
2021-12-15 23:51:37 +0300

    Added Brand and Color entites with an implementation of entity framework

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -10,14 +11,30 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            ColorManager colorManager = new ColorManager(new EfColorDal());
 
-
+            Console.WriteLine("\n");
 
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Id + " " + car.BrandId + " " +
                     car.ColorId + " " + car.ModelYear + " " + car.DailyPrice +
                     " " + car.Description);
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.Id+" "+brand.Name);
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.Id+" "+color.Name);
             }
         }
     }
